12483d0832b3de3fe8dc6257e5ef7be8231dd26c
Zian <zauvohi@gmail>
2017-08-07 16:55:11 -0500

    Updated validations for product price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :brand
   validates :name, :price, :brand, presence: true
-  validates :price, numericality: { less_than: 100 }
+  validates :price, numericality: { less_than_or_equal_to: 100 }
 end
