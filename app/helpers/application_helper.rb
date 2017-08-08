3b639b7f05f0281d048e770b67079cb37d76ff9a
Zian <zauvohi@gmail>
2017-08-08 15:56:23 -0500

    Moved the code to decide which action for a button into action_for_button in application_helper. Added it to brands form partial.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
   def bootstrap_class_for(flash_type)
     BOOTSTRAP_FLASH_CLASSES.fetch(flash_type.to_sym, flash_type.to_s)
   end
+
+  def action_for_button(action)
+    action == 'edit' ? 'Edit' : 'Add'
+  end
 end
